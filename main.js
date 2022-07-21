524ab971df1afdb667274e63377975edd956599f
royrub25 <76081193+royrub25@users.noreply.github.com>
2022-07-21 11:44:33 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -21,9 +21,32 @@ var ball = {
     dy:3
 }
 
-function setup(){
-  var canvas =  createCanvas(700,600);
+
+function setup() {
+	canvas = createCanvas(700, 600);
+	canvas.parent('canvas');
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+	
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
 
 
 function draw(){
